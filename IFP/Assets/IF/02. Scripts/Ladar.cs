5d194fe20be6ef06a98cb70fb4057d9828e847b4
Jin Seong Jun <jsjk300@naver.com>
2018-04-11 15:26:41 +0900

    Add Ladar
    add ladar and set gitignore for save project setting

---
+++ IFP/Assets/IF/02. Scripts/Ladar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladar : MonoBehaviour {
+    public GameObject redDot;
+    public float distance = 1.0f;
+
+    RectTransform map;
+    Transform myTransform;
+    List<GameObject> dots;
+
+    public int dotsCount { get { return dots.Count; } }
+
+    void Start () {
+        map = gameObject.transform.GetChild(0).GetComponent<RectTransform>();
+        myTransform = GameObject.Find("ARCore Device").transform;
+        dots = new List<GameObject>();
+	}
+
+
+    void Update()
+    {
+        var enemies = GameObject.FindGameObjectsWithTag("ENEMY_TYPE01").Select(a => { return a.transform; }).ToList();
+        var gap = dots.Count - enemies.Count;
+        if (gap < 0)
+        {
+            for (int i = 0; i < (-gap); i++)
+            {
+                var newDot = Instantiate(redDot, map);
+                dots.Add(newDot);
+            }
+        }
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            dots[i].SetActive(true);
+            var localPosition = distance * (enemies[i].position - myTransform.position);
+            localPosition.y = localPosition.z;
+            localPosition.z = 0;
+            dots[i].GetComponent<RectTransform>().localPosition = localPosition;
+        }
+        for(int i = enemies.Count; i < dots.Count; i++)
+        {
+            dots[i].SetActive(false);
+        }
+        //var localAngle = new Vector3(0, 0, myTransform.eulerAngles.y);
+        var localAngle = new Vector3(0, 0, myTransform.transform.GetChild(0).eulerAngles.y);
+        map.localEulerAngles = localAngle;
+    }
+}
---
+++ IFP/Assets/IF/07. Debug/Controller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        // start TCP/IP communication
+        const int port = 6670;
+        const string welcomeMessage = "Hello, IFG!";
+        var cli = CLI.Bridge.TryInstall(port, welcomeMessage: welcomeMessage);
+
+        cli.Executer = new CLI.Executer().Bind(typeof(DLadar));
+
+        // bind class
+        //cli.Executer = new CLI.Executer()
+        //    .Bind(typeof(Binding1))
+        //    .Bind(typeof(Binding2));
+    }
+}
+
+public static class DLadar
+{
+    static Ladar ld;
+
+    private static Ladar GetLadar {
+        get {
+            if (ld == null)
+            {
+                ld = GameObject.Find("LdarCircle").GetComponent<Ladar>();
+            }
+            return ld;
+        } }
+
+    [CLI.Bind]
+    public static CLI.Result Distance(string i)
+    {
+        var distance = float.Parse(i, CultureInfo.InvariantCulture.NumberFormat);
+        GetLadar.distance = distance;
+        return CLI.Result.Success("Ladar Distance Set: " + i);
+    }
+
+    [CLI.Bind]
+    public static CLI.Result DotsCount()
+    {
+        return CLI.Result.Success("Dots Count: " + GetLadar.dotsCount);
+    }
+}
