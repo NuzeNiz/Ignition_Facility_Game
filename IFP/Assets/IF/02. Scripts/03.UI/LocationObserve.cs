dd7bcd164552d076278c73c4638fe81f9df5cb56
Jin Seong Jun <jsjk300@naver.com>
2018-06-12 14:16:39 +0900

    GPS, xml data using

---
+++ IFP/Assets/IF/02. Scripts/03.UI/LocationObserve.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationObserve : MonoBehaviour {
+    private Button myBotton;
+    private Image myImage;
+    private Color originColor;
+
+    [SerializeField]
+    private MyLocationChecker MyLocation;
+
+    private void Awake()
+    {
+        myBotton = gameObject.GetComponent<Button>();
+        myImage = gameObject.GetComponent<Image>();
+        originColor = myImage.color;
+    }
+    private void Update()
+    {
+        if (MyLocation.isInGroup)
+        {
+            if (MyLocation.isInLocation)
+            {
+                myImage.color = Color.white;
+            }
+            else
+            {
+                myImage.color = originColor;
+            }
+        }
+        else
+        {
+            myImage.color = originColor;
+        }
+    }
+}
