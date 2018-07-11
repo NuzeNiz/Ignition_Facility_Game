0de91bd40fe9a3902be71eea248c7778afd6ccc5
Jin Seong Jun <jsjk300@naver.com>
2018-07-11 11:43:23 +0900

    Curtain effect and Lader update

---
+++ IFP/Assets/IF/02. Scripts/03.UI/Curtain.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Curtain : MonoBehaviour {
+
+    public bool isFadeOut = true;
+    public bool withStart = true;
+
+    [SerializeField]
+    private float waitSecond = 0.0f;
+
+    private Image myCurtain;
+
+    private void Awake()
+    {
+        myCurtain = gameObject.GetComponent<Image>();
+        if (withStart)
+        {
+            PlayAnimation();
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        if (isFadeOut)
+        {
+            StartCoroutine(CurtainOpen());
+        }
+        else
+        {
+            StartCoroutine(CurtaionOff());
+        }
+    }
+
+    private IEnumerator CurtainOpen()
+    {
+        while (myCurtain.color.a > 0.0f)
+        {
+            myCurtain.color = new Color(myCurtain.color.r, myCurtain.color.g, myCurtain.color.b, myCurtain.color.a - 0.01f);
+            yield return new WaitForSeconds(waitSecond);
+        }
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator CurtaionOff()
+    {
+        gameObject.SetActive(true);
+        while (myCurtain.color.a < 1.0f)
+        {
+            myCurtain.color = new Color(myCurtain.color.r, myCurtain.color.g, myCurtain.color.b, myCurtain.color.a + 0.01f);
+            yield return new WaitForSeconds(waitSecond);
+        }
+    }
+}
--- IFP/Assets/IF/02. Scripts/03.UI/Ladar.cs
+++ IFP/Assets/IF/02. Scripts/03.UI/Ladar.cs
@@ -4,28 +4,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 20180620 SeongJun :
+/// 20180711 SeongJun :
 /// </summary>
-public class Ladar : MonoBehaviour {
+public class Ladar : MonoBehaviour
+{
+    public bool isThereATree = true;
+
+    [SerializeField]
+    private GameObject greenDot;
     public GameObject redDot;
     public float distance = 1.0f;
 
     RectTransform map;
     Transform playerTransform;
+    Transform treeTransform;
     List<GameObject> dots;
+    GameObject onMapTreeDot;
 
     public int dotsCount { get { return dots.Count; } }
 
-    void Start () {
+    void Start()
+    {
         map = gameObject.transform.GetChild(0).GetComponent<RectTransform>();
         playerTransform = GameObject.Find("ARCore Device").transform;
         dots = new List<GameObject>();
-	}
+    }
 
+    private void TreeDisplay()
+    {
+        if (isThereATree)
+        {
+            if ((treeTransform == null) || (onMapTreeDot == null))
+            {
+                var TG = GameObject.FindGameObjectsWithTag("TREE").FirstOrDefault();
+                if (TG == null)
+                {
+                    return;
+                }
+                treeTransform = TG.transform;
+                onMapTreeDot = Instantiate(greenDot, map);
+            }
+            var localPosition = distance * (treeTransform.position - playerTransform.position);
+            localPosition.y = localPosition.z;
+            localPosition.z = 0;
+            onMapTreeDot.GetComponent<RectTransform>().localPosition = localPosition;
+        }
+    }
 
     void Update()
     {
         var enemies = GameObject.FindGameObjectsWithTag("ENEMY_TYPE01").Select(a => { return a.transform; }).ToList();
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE02").Select(a => { return a.transform; }));
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE03").Select(a => { return a.transform; }));
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE01_BOSS").Select(a => { return a.transform; }));
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE02_BOSS").Select(a => { return a.transform; }));
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE03_BOSS").Select(a => { return a.transform; }));
+        enemies.InsertRange(enemies.Count, GameObject.FindGameObjectsWithTag("ENEMY_TYPE04_BOSS").Select(a => { return a.transform; }));
+
         var gap = dots.Count - enemies.Count;
         if (gap < 0)
         {
@@ -35,7 +70,7 @@ public class Ladar : MonoBehaviour {
                 dots.Add(newDot);
             }
         }
-        for(int i = 0; i < enemies.Count; i++)
+        for (int i = 0; i < enemies.Count; i++)
         {
             dots[i].SetActive(true);
             var localPosition = distance * (enemies[i].position - playerTransform.position);
@@ -43,11 +78,13 @@ public class Ladar : MonoBehaviour {
             localPosition.z = 0;
             dots[i].GetComponent<RectTransform>().localPosition = localPosition;
         }
-        for(int i = enemies.Count; i < dots.Count; i++)
+        for (int i = enemies.Count; i < dots.Count; i++)
         {
             dots[i].SetActive(false);
         }
-        //var localAngle = new Vector3(0, 0, myTransform.eulerAngles.y);
+
+        TreeDisplay();
+
         var localAngle = new Vector3(0, 0, playerTransform.transform.GetChild(0).eulerAngles.y);
         map.localEulerAngles = localAngle;
     }
